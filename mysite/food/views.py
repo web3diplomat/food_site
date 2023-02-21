1854bc1540a9dfe7f194b2aa0ff3abdeab452e51
web3diplomat <financialdirection@yahoo.com>
2023-02-21 01:48:21 -0500

    created templates for items to better organize views and used template syntax to pass context and pull items from the db to the html in templates

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    
+    context = {
+        'item_list':item_list,
+    }
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse('This is an item view')
