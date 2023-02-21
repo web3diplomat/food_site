50be318d6ef35f481d4dec13bf7c89d071402e63
web3diplomat <financialdirection@yahoo.com>
2023-02-20 23:05:19 -0500

    created db table for items with item model

---
+++ mysite/food/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello World')
+
+def item(request):
+    return HttpResponse('This is an item view')
