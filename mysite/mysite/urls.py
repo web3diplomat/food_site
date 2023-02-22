5e0781f943e221d601b08cd9dd6f866a64d8f877
web3diplomat <financialdirection@yahoo.com>
2023-02-22 04:03:15 -0500

    created css, load static files, and namespacing

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'food'
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("food/", include('food.urls')),   
+    path("food/", include(('food.urls', 'food'), namespace='food')),   
     
 ]
