988e9de713a060badfbb24a899067897b8a143e9
web3diplomat <financialdirection@yahoo.com>
2023-02-24 01:50:39 -0500

    Create navbar, Create base.html template, Add image field model, Add images for items using admin, design the detail view with bootstrap to include images

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8GV0f6prb-DPesR3lFwG2UsJh94yNz7ijRQ&usqp=CAU")
