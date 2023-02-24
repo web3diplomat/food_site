8dcca61a792e4887e76bfdbfb64755c0fdf566bf
web3diplomat <financialdirection@yahoo.com>
2023-02-24 02:45:30 -0500

    Adding form to add items, Adding base templateto form, Implementing edit function, Implementing delete function

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,10 +1,17 @@
 from . import views
 from django.urls import path 
 
+
 urlpatterns = [
     
 
     path('',views.index,name='index'),
     path('<int:item_id>/',views.detail,name='detail'),
     path('item/',views.item,name='item'),
+    #add items
+    path('add',views.create_item,name='create_item'),
+    #edit
+    path('update/<int:id>/',views.update_item,name='update_item'),
+    #delete
+    path('delete/<int:id>/',views.delete_item,name='delete_item'),
 ]
